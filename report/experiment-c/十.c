816b2365416d5b4d5eef54af4e329ea202fc5030
胡率龙 <921989101@qq.com>
2022-02-21 17:55:51 +0800

    Create 十.c

---
+++ report/experiment-c/十.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    char a=getchar();
+    char b=getchar();
+    char c=getchar();
+    a=a+32;
+    b=b+32;
+    c=c+32;
+    putchar(a);
+    putchar(b);
+    putchar(c);
+    return 0;
+}
